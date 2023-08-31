98514ce33d5d9cf8448a4d8a1b9d4aca659cb1ab
SHIRISH SHETTY <102011980+SHIRISH-30@users.noreply.github.com>
2023-09-01 00:19:42 +0530

    Update chat.js

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -19,7 +19,7 @@ const createChatLi = (message, className) => {
 }
 
 const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const API_URL = "";
     const messageElement = chatElement.querySelector("p");
 
     // Define the properties and message for the API request
